d6ced5e8d4b2008aafcaa4084256c0f3b84834ba
unknown <bhaktiagrawal397@gmail.com>
2024-07-13 03:53:48 +0530

    postform

--- My_Blog_App/src/components/index.js
+++ My_Blog_App/src/components/index.js
@@ -5,6 +5,8 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import Button from './Button'
 import Input from './Input'
+import RTE from "./RTE";
+import Select from './Select'
 
 export{
     Header,
@@ -13,5 +15,7 @@ export{
     Logo, 
     LogoutBtn,
     Button,
-    Input
+    Input,
+    RTE,
+    Select
 }
