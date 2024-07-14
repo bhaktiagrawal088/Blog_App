da311206dcd0d3c75adeb61d7912cca492e83420
unknown <bhaktiagrawal397@gmail.com>
2024-07-15 03:00:30 +0530

    changes to main file

--- My_Blog_App/src/main.jsx
+++ My_Blog_App/src/main.jsx
@@ -4,11 +4,30 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store  from './store/store.js'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import { AuthLayout, Login, SignUp } from './components/index.js'
+import Post from './pages/Post.jsx'
+
+const router = createBrowserRouter([
+  {path : '/', 
+  element: <App/>, 
+  children : [
+    {path: '/', element: <Home/>},
+    {path: '/login', element: (<AuthLayout authentication={false}><Login/></AuthLayout>)},
+    {path : '/signup' , element : (<AuthLayout authentication={false}><SignUp/></AuthLayout>)},
+    {path : '/all-posts', element: (<AuthLayout authentication={" "}><AllPosts/></AuthLayout>)},
+    {path: '/add-post', element: (<AuthLayout authentication={" "}><AddPost/></AuthLayout>)},
+    {path:'/edit-post/:slug', element: (<AuthLayout authentication={" "}><EditPost/></AuthLayout>)},
+    {path: '/post/:slug', element: <Post/>}
+  ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Provider store={store}>
-    <App />
+  <RouterProvider router={router}/>
   </Provider>
   </React.StrictMode>,
 )
